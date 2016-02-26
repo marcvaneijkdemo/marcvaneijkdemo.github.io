a50ec9e7a90dd545754b8ebf202671d42fd3f042
marcvaneijkdemo <marcvaneijkdemo@outlook.com>
2016-02-26 17:26:56 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,13 +1,15 @@
 jQuery(function() {
-  // Initalize lunr with the fields it will be searching on. I've given title
-  // a boost of 10 to indicate matches on this field are more important.
+  // Initialize lunr with the fields to be searched, plus the boost.
   window.idx = lunr(function () {
-    this.field('title', { boost: 10 });
-    this.field('content');
+    this.field('id');
+    this.field('title');
+    this.field('content', { boost: 10 });
+    this.field('author');
+    this.field('categories');
   });
 
-  // Download the data from the JSON file we generated
-  window.data = $.getJSON('/search.json');
+  // Get the generated search_data.json file so lunr.js can search it locally.
+  window.data = $.getJSON('/search_data.json');
 
   // Wait for the data to load and add it to lunr
   window.data.then(function(loaded_data){
@@ -19,7 +21,7 @@ jQuery(function() {
   });
 
   // Event when the form is submitted
-  $("#site_search").submit(function(){
+  $("#site_search").submit(function(event){
       event.preventDefault();
       var query = $("#search_box").val(); // Get the value for the text field
       var results = window.idx.search(query); // Get lunr to perform a search
@@ -43,11 +45,12 @@ jQuery(function() {
           // Build a snippet of HTML for this result
           var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
 
-          // Add it to the results
+          // Add the snippet to the collection of results.
           $search_results.append(appendString);
         });
       } else {
-        $search_results.html('<li>No results found</li>');
+        // If there are no results, let the user know.
+        $search_results.html('<li>No results found.<br/>Please check spelling, spacing, yada...</li>');
       }
     });
   }
