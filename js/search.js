7363f957384b00d7fd12a83c3b0298e53d3989da
marcvaneijkdemo <marcvaneijkdemo@outlook.com>
2016-02-26 17:09:38 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -2,10 +2,9 @@ jQuery(function() {
   // Initalize lunr with the fields it will be searching on. I've given title
   // a boost of 10 to indicate matches on this field are more important.
   window.idx = lunr(function () {
-    this.field('id');
     this.field('title', { boost: 10 });
-    this.field('author');
     this.field('category');
+    this.field('content');
   });
 
   // Download the data from the JSON file we generated
